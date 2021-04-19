019fceb8bb9f0c17e4c799f8e16376399ac779b1
John Winstead <brooswit@gmail.com>
2021-04-18 17:43:35 -0700

    style update, bugfix
    was calling parseAsync, which doesn't exist anymore

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 const SafeJSON = {
   parse(json, optionalFallback = undefined, optionalCallback = undefined) {
-    let error = null
-    let value = optionalFallback
+    let error = null;
+    let value = optionalFallback;
     try {
-      value = JSON.parse(json)
-    } catch (ex) {
-      error = ex
+      value = JSON.parse(json);
+    } catch (err) {
+      error = err;
     }
-    if (optionalCallback) optionalCallback(error, value)
-    return value
+    if (optionalCallback) optionalCallback(error, value);
+    return value;
   },
 
   stringify(value, optionalCallback = undefined) {
-    let error = null
+    let error = null;
     try {
-      JSON.stringify(value)
-    } catch (ex) {
-      error = ex
+      JSON.stringify(value);
+    } catch (err) {
+      error = err;
     }
-    if (optionalCallback) optionalCallback(error)
+    if (optionalCallback) optionalCallback(error);
   },
 
   isValid(json, optionalCallback = undefined) {
-    const token = {}
-    const callbackHandler = !optionalCallback ? undefined : (error, json)=>{
-      const valid = json !== token
-      if (optionalCallback) optionalCallback(error, valid)
+    const token = {};
+    const callbackHandler = !optionalCallback ? undefined : (error, json) => {
+      const valid = json !== token;
+      if (optionalCallback) optionalCallback(error, valid);
     }
-    const valid = this.parseSync(json, token, callbackHandler) !== token
-    return valid
+    const valid = this.parse(json, token, callbackHandler) !== token;
+    return valid;
   }
 }
 
-
-
-module.exports = SafeJSON
+module.exports = SafeJSON;
